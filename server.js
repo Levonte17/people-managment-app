7aad4bfcdfe7b37ae6438174edcc3966fd0e684c
Levonte Larry <levontelarry@Levontes-MacBook-Air.local>
2022-09-23 19:39:46 -0400

    logger and middleware

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //DEPENDENCIES
 const express = require('express');
 const mongoose = require('mongoose');
-
+const logger = require('morgan')
 //INITIALIZE
 const app = express();
 //CONFIG
@@ -11,7 +11,18 @@ const {PORT = 4000, DATABASE_URL} = process.env;
     mongoose.connect(DATABASE_URL);
     mongoose.connection
     .on('connected', () => console.log('Mongoose Connected, Lets Gooo!!'))
+// SCHEMA MODEL
+const peopleSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    title: String,
+}, {timestamps: true}); //APROX
+    //Model
+    const People = mongoose.model('People', peopleSchema);
+
 //MIDDLEWARE
+app.use(express.json());     //interceptjson
+app.use(logger('dev'));
 //ROUTES
 app.get('/', (req, res) => {
 res.send('Welcome people')
